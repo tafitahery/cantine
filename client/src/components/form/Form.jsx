3c8f48dd71167bc7325bc1f9ad5af1432c5814b3
Tafita Hery <tafitahery@gmail.com>
2023-03-24 10:58:43 +0300

    Page Home

--- client/src/components/form/Form.jsx
+++ client/src/components/form/Form.jsx
@@ -1,5 +1,38 @@
 import './form.scss';
 
 export default function Form() {
-  return <div className="form">Form</div>;
+  return (
+    <div className="form">
+      <form>
+        <div className="inputForm">
+          <label htmlFor="">Date:</label>
+          <input type="date" />
+        </div>
+        <div className="inputForm">
+          <label htmlFor="">Code Client:</label>
+          <input type="text" />
+        </div>
+        <div className="inputForm">
+          <label htmlFor="">Nom:</label>
+          <input type="text" />
+        </div>
+        <div className="inputForm">
+          <label htmlFor="">Menu:</label>
+          <select>
+            <option value="m1">M1</option>
+            <option value="D">M2</option>
+            <option value="m3">M3</option>
+          </select>
+        </div>
+        <div className="inputForm">
+          <label htmlFor="">Accompagnement:</label>
+          <select>
+            <option value="m1">E</option>
+            <option value="m2">D</option>
+          </select>
+        </div>
+        <button>Valider</button>
+      </form>
+    </div>
+  );
 }
